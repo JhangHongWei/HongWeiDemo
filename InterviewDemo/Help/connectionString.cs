65ceb398226bc549ea08b477b456c4de9a87e28b
HongWei <hongwei-65@hotmail.com>
2018-05-13 13:48:12 +0800

    專案基本架構

---
+++ InterviewDemo/Help/connectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace InterviewDemo.Help
+{
+    public class connectionString
+    {
+        public static string DemoDB { get { return WebConfigurationManager.ConnectionStrings["DemoDB"].ConnectionString; } }
+    }
+}
